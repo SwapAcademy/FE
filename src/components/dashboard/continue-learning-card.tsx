6936577c0320ccb02fe62ba197bb9698daf1005a
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-10-03 00:06:25 +0700

    Remove unused routes

--- src/components/dashboard/continue-learning-card.tsx
+++ src/components/dashboard/continue-learning-card.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Progress } from '@/components/ui/progress';
 import {
   Table,
   TableBody,
@@ -10,8 +11,6 @@ import {
 } from '@/components/ui/table';
 import { ChevronRight, Hourglass } from 'lucide-react';
 import Link from 'next/link';
-import { Progress } from '@/components/ui/progress';
-import { VideoThumbnail } from '@/components/mentor/my-videos/my-videos-table';
 
 let placeholder = [
   {
@@ -84,5 +83,23 @@ export default function ContinueLearningCard() {
         </div>
       </CardContent>
     </Card>
+  );
+}
+
+function VideoThumbnail() {
+  return (
+    <svg
+      width="91"
+      height="45"
+      viewBox="0 0 91 45"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="0.5" width="90" height="45" rx="4" fill="#D9D9D9" />
+      <path
+        d="M45.5 12.75C43.5716 12.75 41.6866 13.3218 40.0832 14.3932C38.4798 15.4645 37.2301 16.9873 36.4922 18.7688C35.7542 20.5504 35.5611 22.5108 35.9373 24.4021C36.3136 26.2934 37.2422 28.0307 38.6057 29.3943C39.9693 30.7579 41.7066 31.6865 43.5979 32.0627C45.4892 32.4389 47.4496 32.2458 49.2312 31.5078C51.0127 30.7699 52.5355 29.5202 53.6068 27.9168C54.6782 26.3134 55.25 24.4284 55.25 22.5C55.2473 19.915 54.2192 17.4366 52.3913 15.6087C50.5634 13.7808 48.085 12.7527 45.5 12.75ZM49.3016 23.1169L44.4266 26.4919C44.314 26.5697 44.1823 26.6152 44.0458 26.6236C43.9092 26.6319 43.7729 26.6028 43.6517 26.5392C43.5306 26.4757 43.4291 26.3802 43.3583 26.2631C43.2874 26.1461 43.25 26.0118 43.25 25.875V19.125C43.25 18.9882 43.2874 18.8539 43.3583 18.7369C43.4291 18.6198 43.5306 18.5243 43.6517 18.4608C43.7729 18.3972 43.9092 18.3681 44.0458 18.3764C44.1823 18.3848 44.314 18.4303 44.4266 18.5081L49.3016 21.8831C49.4013 21.9521 49.4829 22.0443 49.5392 22.1517C49.5956 22.2592 49.625 22.3787 49.625 22.5C49.625 22.6213 49.5956 22.7408 49.5392 22.8483C49.4829 22.9557 49.4013 23.0479 49.3016 23.1169Z"
+        fill="white"
+      />
+    </svg>
   );
 }
